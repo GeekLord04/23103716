a901638e57e22c07aede8eb478a24d94f4001f70
Chirag Chakraborty <chiragofficial04@gmail.com>
2024-09-03 23:58:18 +0530

    Add files via upload

---
+++ src/Utils/Helper.java
@@ -0,0 +1,41 @@
+package Utils;
+
+import DAO.DatabaseConnection;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Helper {
+    public static void resetAutoIncrement(String tableName, int startValue) {
+        String query = "ALTER TABLE " + tableName + " AUTO_INCREMENT = " + startValue;
+        try (Connection con = DatabaseConnection.getConnection();
+             Statement stmt = con.createStatement()) {
+            stmt.executeUpdate(query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean isTableEmpty(String tableName) {
+        boolean isEmpty = true;
+        String query = "SELECT COUNT(*) FROM " + tableName;
+
+        try (Connection con = DatabaseConnection.getConnection();
+             Statement stmt = con.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+
+            if (rs.next()) {
+                int count = rs.getInt(1);
+                if (count > 0) {
+                    isEmpty = false;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return isEmpty;
+    }
+}
